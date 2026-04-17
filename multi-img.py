9091c7393d9494d882ad5fb5bc2b41b48ab3bac6
綾波あやなみ <2574703489@qq.com>
2026-04-17 22:45:05 +0800

    add multi-img.py

---
+++ multi-img.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+plt.figure()
+
+plt.subplot(3,2,1)
+plt.plot([0,1],[0,1])
+
+plt.subplot(3,2,2)
+plt.plot([0,4],[-3,-1])
+
+plt.subplot(2,2,3)
+plt.plot([2,6],[-1,3])
+
+plt.subplot(4,2,8)
+plt.plot([0,1],[0,3])
+plt.show()
